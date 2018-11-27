d72c3a3a8a87778addcbbc21f3fb4622f721b762
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-27 00:59:24 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 require_relative '../lib/concerns2/findable'
 require_relative '../lib/concerns2/memorable'
-# require_relative '../lib/concerns2/paramable'
+require_relative '../lib/concerns2/paramable'
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
